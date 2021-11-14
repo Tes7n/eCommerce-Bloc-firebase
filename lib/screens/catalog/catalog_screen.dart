adbe02406db5b05a44d42f2b000189709bcd5a17
Text('Tushar') <tesvn7@gmail.com>
2021-11-14 15:41:48 +0530

    product screen ui

--- lib/screens/catalog/catalog_screen.dart
+++ lib/screens/catalog/catalog_screen.dart
@@ -1,24 +1,51 @@
+import 'package:ecommerce_bloc/models/category_model.dart';
+import 'package:ecommerce_bloc/models/models.dart';
 import 'package:ecommerce_bloc/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class CatalogScreen extends StatelessWidget {
-  const CatalogScreen({Key? key}) : super(key: key);
+  final Category category;
+  const CatalogScreen({Key? key, required this.category}) : super(key: key);
   static const String routeName = '/catalog';
-  static Route route() {
+  static Route route({required Category category}) {
     return MaterialPageRoute(
       // ignore: prefer_const_constructors
       settings: RouteSettings(name: routeName),
       // ignore: prefer_const_constructors
-      builder: (_) => CatalogScreen(),
+      builder: (_) => CatalogScreen(
+        category: category,
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    final List<Product> categoryProducts = Product.products
+        .where((element) => element.category == category.name)
+        .toList();
     return Scaffold(
-      appBar: CustomAppBar(title: 'Catalog'),
+      appBar: CustomAppBar(title: category.name),
       // ignore: prefer_const_constructors
       bottomNavigationBar: CustomNavBar(),
+      body: GridView.builder(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 8,
+          vertical: 16,
+        ),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.1,
+        ),
+        itemCount: categoryProducts.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Center(
+            child: ProductCard(
+              product: categoryProducts[index],
+              widthFactor: 2.2,
+            ),
+          );
+        },
+      ),
     );
   }
 }
--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -3,83 +3,95 @@ import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
+  final double widthFactor;
   const ProductCard({
     Key? key,
     required this.product,
+    this.widthFactor = 2.5,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          width: MediaQuery.of(context).size.width / 2.5,
-          height: 150,
-          child: Image.network(
-            product.imageUrl,
-            fit: BoxFit.cover,
-          ),
-        ),
-        Positioned(
-          bottom: 1,
-          child: Container(
-            width: MediaQuery.of(context).size.width / 2.5,
-            height: 60,
-            decoration: BoxDecoration(
-              color: Colors.black.withAlpha(50),
+    final double widthValue = MediaQuery.of(context).size.width / widthFactor;
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          '/product',
+          arguments: product,
+        );
+      },
+      child: Stack(
+        children: [
+          Container(
+            width: widthValue,
+            height: 150,
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
             ),
           ),
-        ),
-        Positioned(
-          bottom: 1,
-          child: Container(
-            width: MediaQuery.of(context).size.width / 2.5 - 10,
-            height: 50,
-            decoration: BoxDecoration(
-              color: Colors.black,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 3,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '${product.name}',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline5!
-                              .copyWith(color: Colors.white),
-                        ),
-                        Text(
-                          '\$${product.price}',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline6!
-                              .copyWith(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.add_circle,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
+          Positioned(
+            bottom: 1,
+            child: Container(
+              width: widthValue,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.black.withAlpha(50),
               ),
             ),
           ),
-        ),
-      ],
+          Positioned(
+            bottom: 1,
+            child: Container(
+              width: widthValue - 10,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${product.name}',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline5!
+                                .copyWith(color: Colors.white),
+                          ),
+                          Text(
+                            '\$${product.price}',
+                            style: Theme.of(context)
+                                .textTheme
+                                .headline6!
+                                .copyWith(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.add_circle,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
